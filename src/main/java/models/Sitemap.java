4428f7720837310c396771d2c0c53232c11fffea
jmusau62-gmail <jmusau62@gmail.com>
2022-01-20 17:17:55 +0300

    added site map info

--- src/main/java/models/Sitemap.java
+++ src/main/java/models/Sitemap.java
@@ -1,4 +1,25 @@
+
 package models;
 
 public class Sitemap {
+    private String describe;
+    private String path;
+    private String id;
+
+    public Sitemap(String describe, String path) {
+        this.describe = describe;
+        this.path = path;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
