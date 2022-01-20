9697b36880b4d49e27430065e20130b205f462f2
jmusau62-gmail <jmusau62@gmail.com>
2022-01-20 16:43:21 +0300

    created sql.file

---
+++ src/create.sql
@@ -0,0 +1,8 @@
+--CREATE DATABASE news_portal;
+--\c news_portal
+--
+--CREATE TABLE departments(id SERIAL PRIMARY KEY, name VARCHAR,description VARCHAR,totalEmployees INT);
+--
+--CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR,position VARCHAR,role VARCHAR,department VARCHAR);
+--
+--CREATE TABLE news(id SERIAL PRIMARY KEY, title VARCHAR, description VARCHAR, type VARCHAR, author VARCHAR);
