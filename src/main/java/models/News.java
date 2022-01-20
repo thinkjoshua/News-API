b8288dbce90680c0fa6b23731687d2249dab9add
jmusau62-gmail <jmusau62@gmail.com>
2022-01-20 17:20:14 +0300

    added news info

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,74 @@
+
 package models;
 
+import java.util.Objects;
+
 public class News {
+    private String title;
+    private String description;
+    private String type;
+    private String author;
+    private int id;
+
+    public News(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.type = "General";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        News news = (News) o;
+        return id == news.id &&
+                Objects.equals(title, news.title) &&
+                Objects.equals(description, news.description) &&
+                Objects.equals(type, news.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, description, type, id);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
